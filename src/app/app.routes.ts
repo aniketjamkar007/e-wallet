50217d212d98dd7681711ae35bce5096dafafb68
aniketjamkar007 <aniketjamkar@gmail.com>
2025-06-17 17:02:30 +0530

    scaffolding an app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'wallet', pathMatch: 'full' },
+    { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+    { path: 'wallet', loadChildren: () => import('./wallet/wallet.module').then(m => m.WalletModule) },
+    { path: 'expenses', loadChildren: () => import('./expenses/expenses.module').then(m => m.ExpensesModule) },
+];
---
+++ src/app/wallet/send-money/send-money.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-send-money',
+  imports: [],
+  templateUrl: './send-money.component.html',
+  styleUrl: './send-money.component.scss'
+})
+export class SendMoneyComponent {
+
+}
